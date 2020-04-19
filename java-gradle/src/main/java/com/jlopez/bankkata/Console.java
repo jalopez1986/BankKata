0e21a28028557c2112bb3360d5b07f64cf6b5d8e
Jorge A Lopez <jlobeto@gmail.com>
2020-04-19 18:50:37 -0500

    Se hacen ajuste finales

--- java-gradle/src/main/java/com/jlopez/bankkata/Console.java
+++ java-gradle/src/main/java/com/jlopez/bankkata/Console.java
@@ -1,6 +1,7 @@
 package com.jlopez.bankkata;
 
 public class Console {
-    public void printLine(String s) {
+    public void printLine(String text) {
+        System.out.println(text);
     }
 }
--- java-gradle/src/test/java/com/feature/PrintStatementFeature.java
+++ java-gradle/src/test/java/com/feature/PrintStatementFeature.java
@@ -8,6 +8,7 @@ import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.verify;
 
@@ -27,6 +28,8 @@ public class PrintStatementFeature {
 
     @Test
     public void print_statement_containing_all_transaction() {
+        given(clock.todayAsString()).willReturn("01/04/2020", "02/04/2020", "10/04/2020");
+
         account.deposit(1000);
         account.withdraw(100);
         account.deposit(500);
@@ -37,6 +40,6 @@ public class PrintStatementFeature {
         inOrder.verify(console).printLine("DATE | AMOUNT | BALANCE");
         inOrder.verify(console).printLine("10/04/2020 | 500.00 | 1400.00");
         inOrder.verify(console).printLine("02/04/2020 | -100.00 | 900.00");
-        inOrder.verify(console).printLine("10/04/2020 | 1000.00 | 1000.00");
+        inOrder.verify(console).printLine("01/04/2020 | 1000.00 | 1000.00");
     }
 }
